23fb5e188496506423ef7a6fcf56b2cdfec3b572
hybras <24651269+hybras@users.noreply.github.com>
2021-11-21 22:07:50 -0500

    Function parse labels and arg parsing

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,5 +1,46 @@
-pub mod consts;
+pub mod instr;
 
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, fs::File, io::{BufRead, BufReader, BufWriter}, path::PathBuf};
+
+use argh::FromArgs;
+use anyhow::Result;
+
+/// error: usage: %s <assembly-code-file> <machine-code-file>
+#[derive(FromArgs)]
+struct Args {
+    /// the input mips assembly file
+    #[argh(option, short = 'i')]
+    input: PathBuf,
+    /// the assembled machine code file
+    #[argh(option, short = 'o')]
+    output: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let Args {input, output} = argh::from_env::<Args>();
+    let input = File::open(input)?;
+    let output = File::open(output)?;
+    let output = BufWriter::new(output);
+    
+    // First Pass
+    let labels = get_labels(&input);
+
+    Ok(())
+}
+
+fn get_labels(input: &File) -> HashMap::<String, u32> {
+    let input = BufReader::new(input);
+    input
+    .lines()
+    .enumerate()
+        .filter_map(|(line_num, line)| {
+            let line = line.unwrap();
+            let is_labeled = line.starts_with("\t");
+            if is_labeled {
+                Some((line.split_once("\t").unwrap().0.to_owned(), (line_num as u32) << 4))
+            } else {
+                None
+            }
+        })
+        .collect()
 }
